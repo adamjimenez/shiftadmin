fc84638f5586ce7e22ce2158a37520a2ebd6a9a9
Adam Jimenez <adam.jimenez@gmail.com>
2024-03-09 09:53:35 +0000

    page layout

--- src/components/UpgradePage.vue
+++ src/components/UpgradePage.vue
@@ -3,7 +3,7 @@
         <v-card class="d-flex flex-column">
             <v-card-text>
 
-                <div class="text-h1 mb-5">
+                <div class="text-h2 mb-5">
                     Effortless admin
                 </div>
 
@@ -30,65 +30,44 @@
             </v-row>
 
             <v-row>
-                <v-col>
-                    <div class="text-h4 mb-5">
-                        Starter
-                    </div>
-                    <ul>
-                        <li>CMS</li>
-                        <li>Configuration area</li>
-                        <li>File uploads</li>
-                        <li>Search/ Advanced search</li>
-                        <li>Event logs</li>
-                    </ul>
-                </v-col>
-                <v-col>
-                    <div class="text-h4 mb-5">
-                        <v-badge color="error" content="Popular">
-                            Basic
-                        </v-badge>
-                    </div>
-                    <ul>
-                        <li>Everytbing in Starter</li>
-                        <li>Import/ Export</li>
-                        <li>Custom CMS buttons</li>
-                        <li>Menu navigation builder (coming soon!)</li>
-                    </ul>
-                </v-col>
-                <v-col>
-                    <div class="text-h4 mb-5">
-                        Business
-                    </div>
-                    <ul>
-                        <li>Everytbing in Basic</li>
-                        <li>User privileges</li>
-                        <li>Bulk editing</li>
-                        <li>Reporting tool (coming soon!)</li>
-                    </ul>
-                </v-col>
-            </v-row>
-            <v-row>
                 <v-col v-for="edition in editions" :key="edition">
-                    <span class="text-h3" :style="edition.hosted ? 'text-decoration-line: line-through;' : ''">£{{ monthly ? edition.monthlyPricing
-                    : edition.annualPricing }}</span> /mo</v-col>
-            </v-row>
-            <v-row>
-                <v-col v-for="edition in editions" :key="edition">
-                    Save {{ Math.round(100 * ((edition.monthlyPricing - edition.annualPricing) / edition.monthlyPricing))
-                    }}% annually
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col v-for="edition in editions" :key="edition">
-                    <div v-if="edition.name === data.edition">
-                        Your current version
-                            
-                        <p v-if="edition.hosted" style="font-size: small; font-weight: bold;" class="mt-1">
-                            Hosting discount applied
-                        </p>
-                    </div>
-                    <v-btn v-else target="_blank"
-                        :href="'https://www.paypal.com/cgi-bin/webscr?cmd=_xclick-subscriptions&business=adam%40shiftcreate%2ecom&item_name=GenieAdmin%20' + edition.name + (monthly ? '' : '%20(year)') + ' &item_number=' + data.id + '&no_shipping=1&no_note=1&currency_code=GBP&lc=GB&bn=PP%2dSubscriptionsBF&ap1=1&a3=' + (monthly ? edition.monthlyPricing : edition.annualPricing * 12) + '&p3=1&t3=Y&src=1&sra=1&notify_url=https:%2F%2Fgenieadmin.com%2Fpaypal'">Choose</v-btn>
+                    <v-card style="min-height: 340px;">
+                        <v-card-title>
+                            <div class="text-h4 mb-5">
+                                {{ edition.name }}
+                            </div>
+                        </v-card-title>
+                        <v-card-text>
+                            <ul style="min-height: 100px;" class="mx-3">
+                                <li v-for="feature in edition.features" :key="feature">{{ feature }}</li>
+                            </ul>
+
+                            <div class="my-3">
+                                <span class="text-h3"
+                                    :style="edition.hosted ? 'text-decoration-line: line-through;' : ''">£{{
+                                        monthly ? edition.monthlyPricing : edition.annualPricing }}</span> /mo
+                            </div>
+
+                            <div>
+                                Save {{ Math.round(100 * ((edition.monthlyPricing - edition.annualPricing) /
+                                    edition.monthlyPricing))
+                                }}% annually
+                            </div>
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <div v-if="edition.name === data.edition">
+                                Your current version
+
+                                <p v-if="edition.hosted" style="font-size: small; font-weight: bold;" class="mt-1">
+                                    Hosting discount applied
+                                </p>
+                            </div>
+                            <v-btn v-else color="primary" variant="flat" target="_blank"
+                                :href="'https://www.paypal.com/cgi-bin/webscr?cmd=_xclick-subscriptions&business=adam%40shiftcreate%2ecom&item_name=GenieAdmin%20' + edition.name + (monthly ? '' : '%20(year)') + ' &item_number=' + data.id + '&no_shipping=1&no_note=1&currency_code=GBP&lc=GB&bn=PP%2dSubscriptionsBF&ap1=1&a3=' + (monthly ? edition.monthlyPricing : edition.annualPricing * 12) + '&p3=1&t3=Y&src=1&sra=1&notify_url=https:%2F%2Fgenieadmin.com%2Fpaypal'">Choose</v-btn>
+                        </v-card-actions>
+
+                    </v-card>
                 </v-col>
             </v-row>
 
@@ -114,16 +93,35 @@ export default {
                 name: 'Starter',
                 monthlyPricing: 8,
                 annualPricing: 5,
+                features: [
+                    'CMS',
+                    'Configuration area',
+                    'File uploads',
+                    'Search/ Advanced search',
+                    'Event logs',
+                ],
             },
             {
                 name: 'Basic',
                 monthlyPricing: 14,
                 annualPricing: 10,
+                features: [
+                    'Everytbing in Starter',
+                    'Import/ Export',
+                    'Custom CMS buttons',
+                    'Menu navigation builder (coming soon!)',
+                ],
             },
             {
                 name: 'Business',
-                monthlyPricing: 25,
-                annualPricing: 20,
+                monthlyPricing: 60,
+                annualPricing: 50,
+                features: [
+                    'Everytbing in Basic',
+                    'User privileges',
+                    'Bulk editing',
+                    'Reporting tool Beta',
+                ],
             },
         ]
     }),

d96066bad062132be95c235771fba74145ed4edb
Adam Jimenez <adam.jimenez@gmail.com>
2024-03-31 14:09:59 +0100

    2fa

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -4,22 +4,52 @@
             <v-alert v-if="error" type="error" :text="error" />
             <v-alert v-if="message" type="info" :text="message" />
 
-            <v-card-text v-if="data.code !== 1 && data.code !== 2 && error !== 'invalid'">
-                <div v-if="data.code !== 2 && error !== 'invalid'">
+            <v-card-text v-if="data.code !== 2 && error !== 'invalid'">
+                <div v-if="data.trusted === false">
+                    <div v-if="!data.otp_sent">
+                        <v-radio-group v-model="otpMethod" class="mb-3">                            
+                            <v-radio value="email">
+                                <template v-slot:label>
+                                    <div>
+                                        <div class="mb-2">Email me a code</div>
+                                        <strong>{{data.email}}</strong>
+                                    </div>
+                                </template>
+                            </v-radio>
+                        </v-radio-group>
+                    </div>
+                    <div v-else>
+                        <p>
+                            An OTP has been sent to you, please enter the code below.
+                        </p>
+
+                        <v-otp-input
+                            v-model="otp"
+                            :disabled="loading"
+                            @finish="fetchData"
+                            :length="otpLength"
+                            autofocus
+                            type="password"
+                            ></v-otp-input>
+                    </div>
+                </div>
+
+                <div v-if="data.code !== 1 && data.code !== 2 && error !== 'invalid'">
                     <v-text-field v-if="!query.code" v-model="email" :rules="usernameRules" label="Email"
                         required></v-text-field>
                     <v-text-field v-if="!reset || query.code" type="password" v-model="password" :rules="passwordRules"
                         label="Password" required></v-text-field>
-                    <v-btn v-if="!reset && !query.code" @click="reset = true" variant="text">Forgot password</v-btn>
+                    <v-btn v-if="!reset && !query.code && !data.no_admin" @click="reset = true" variant="text">Forgot password</v-btn>
                 </div>
             </v-card-text>
 
             <v-card-actions>
-                <v-btn v-if="data.code === 1" @click="logout">Logout</v-btn>
-                <v-btn v-else-if="data.code !== 2" color="success" @click="fetchData()" variant="flat"
+                <v-btn v-if="data.code !== 2" color="success" @click="fetchData()" variant="flat"
                     :text="submitText" type="button" :disabled="!isValid" />
                 <v-btn v-if="reset && data.code !== 2" @click="reset = false" :to="base + 'login'"
                     variant="text">Cancel</v-btn>
+                    <v-spacer></v-spacer>
+                <v-btn v-if="data.code === 1" @click="logout">Logout</v-btn>
             </v-card-actions>
         </v-card>
     </v-form>
@@ -48,6 +78,10 @@ export default {
         query: {},
         data: {},
         reset: false,
+        otpMethod: "",
+        otpSent: false,
+        otpLength: 4,
+        otp: '',
     }),
 
     methods: {
@@ -63,7 +97,9 @@ export default {
                 email: this.email,
                 password: this.password,
                 query: this.query,
-                reset: this.reset
+                reset: this.reset,
+                otp: this.otp,
+                otp_method: this.otpMethod,
             };
 
             this.error = '';
@@ -83,7 +119,7 @@ export default {
                 }
 
                 if (this.data.code === 1) {
-                    if (result.data.admin) {
+                    if (result.data.admin && result.data.trusted !== false) {
                         this.$router.push(util.base());
                     } else if (this.reset) {
                         this.reset = false;
@@ -128,6 +164,10 @@ export default {
                 return 'Set password and login';
             } else if (this.reset) {
                 return 'Send password reminder';
+            } else if (this.data.trusted === false) {
+                return 'Next';
+            } else if (this.data.no_admin) {
+                return 'Create account';
             }
 
             return 'Log in';

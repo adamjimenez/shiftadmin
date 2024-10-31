d9c1e47469e07760adc046f0bf79e3cfc70ad76f
Adam Jimenez <adam.jimenez@gmail.com>
2024-10-31 13:27:38 +0000

    fix path

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://admin.genieadmin.com/' : '',
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://admin.genieadmin.com/' : '/',
   transpileDependencies: true,
   pluginOptions: {
     vuetify: {}
-  }
+  },
 })
